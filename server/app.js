ce784cce73d20cd22ae3700f18dc1d861b580179
Roderick Romero <rjrr507@gmail.com>
2019-04-30 18:09:05 -0500

    creacion de servidor en app.js

--- server/app.js
+++ server/app.js
@@ -1,10 +1,26 @@
 //  app PetLink
 
 const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
 
 const routers = require('./routes/index');
 
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+const PUBLIC_FILES = path.join(__dirname, 'public');
+const VIEWS = path.join(__dirname, 'views');
+
+app.set('port', PORT);
+
+// seteando direcciones estaticas en el servidor
+app.set('views', VIEWS);
+app.set('view engine', 'pug');
+app.use(express.static(PUBLIC_FILES));
+
+// middlewares
+app.use(morgan('common'));
 app.use(routers);
-app.listen(3000);
+
+app.listen(PORT, () => console.info(`server ready on http://localhost:${PORT}`));
