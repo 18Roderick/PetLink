c0e5d805c07eaac4561a7b6e4f50481e8a14cdac
roderick romero <rjrr507@gmail.com>
2019-06-30 19:35:21 -0500

    reset project

--- server/middleware/isAuthenticated.js
+++
@@ -1,8 +0,0 @@
-const isAuthenticated = (req, res, next) => {
-  if (req.isAuthenticated()) {
-    next();
-  }
-  res.redirect('/login/signin');
-};
-
-module.exports = { isAuthenticated };
--- server/routes/admin.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-
-router.get('admin', (req, res) => {
-  res.send('sitio de administrador');
-});
-
-module.exports = router;
